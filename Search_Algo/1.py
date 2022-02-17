a2d8653860a14d920bb94b38cea53699c5464c3e
Peshraw Sarwar <peshrawsarwar@gmail.com>
2022-02-17 13:31:51 +0300

    Seach Algo

---
+++ Search_Algo/1.py
@@ -0,0 +1,27 @@
+graph = {
+    'B': [('A', 12), ('C', 10), ('E', 14)],
+    'A': [('D', 6)],
+    'D': [('H', 3)],
+    'H': [('G', 0)],
+    'C': [('D', 6), ('F', 4)],
+    'F': [('H', 3)],
+    'E': [('F', 4)],
+}
+
+
+def greedy(start, goal, graph, queue=[], visited=[]):
+    if start not in visited:
+        print(start)
+        visited.append(start)
+
+    queue = queue + [i for i in graph[start] if i[0][0] not in visited]
+    queue.sort(key=lambda x: x[1])
+    if queue[0][0][0] == goal:
+        print(queue[0][0])
+    else:
+        p=queue[0]
+        queue.remove(p)
+        greedy(p[0], goal, graph, queue, visited)
+
+greedy('B', 'G', graph)
+
